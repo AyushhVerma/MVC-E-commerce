fdd84bda5b6bbe7b3b2256a19cd34adb42214bf2
Ayush Verma <54748755+AyushhVerma@users.noreply.github.com>
2024-03-21 16:20:54 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,24 @@
+﻿select * from EStoreProDB
+
+
+insert into admin_Login(EmpID,UserName,Password,RoleType,Role_RoleID) values(2,'sai','1234',1,1)
+select * from admin_Login
+
+insert into admin_Employee(FirstName,LastName,DateofBirth,Gender,Email,Address,Phone) values('Sai Hareesh','Kumar','2001-12-23','Male','sai@mail.com','hyd','12312312')
+select * from admin_Employee
+
+delete from admin_Employee
+where EmpID=3
+
+
+insert into Roles values('Admin','Up above the world')
++
+alter table admin_Employee
+alter column Phone varchar(max) not null
+
+
+select * from Customers
+select * from Products
+
+update table Products 
+where ProductID=5 set Name="Converse T-Shirt"
